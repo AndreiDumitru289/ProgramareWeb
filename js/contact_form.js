53f55c43ad17c24c283942d3cf05b77b875fdada
Daniel Andrei Dumitru <daniel-andrei.dumitru@student.unitbv.ro>
2026-03-04 14:27:14 +0200

    Lab 1: HTML and CSS review

---
+++ js/contact_form.js
@@ -0,0 +1,6 @@
+function submitForm(){
+    const nume = document.getElementById("lname").value;
+    const prenume = document.getElementById("fname").value;
+    console.log(nume, " ", prenume);
+    console.warn("Goodbye World!");
+}
