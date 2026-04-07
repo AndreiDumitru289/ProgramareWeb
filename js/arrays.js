f50650ad18bb77eeafc47887215046ad19d0330f
Daniel Andrei Dumitru <daniel-andrei.dumitru@student.unitbv.ro>
2026-04-07 15:46:18 +0300

    feat: React and Vite setup

--- js/arrays.js
+++
@@ -1,3 +0,0 @@
-const elementeLista = document.querySelectorAll('#education li');
-const arrayEducatie = Array.from(elementeLista).map(element => element.textContent.trim());
-console.log(arrayEducatie);
