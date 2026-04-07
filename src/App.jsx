05f51fe668871f2cd20983d95c3c8d2c3980aa4a
Daniel Andrei Dumitru <daniel-andrei.dumitru@student.unitbv.ro>
2026-04-07 16:25:13 +0300

    ex.4

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
 import Card from './Card';
 
 function App() {
+
+  const projects = [
+ { title: "Proiect 1", description: "Pagina personala" },
+ { title: "Proiect 2", description: "Calculator buget" },
+ { title: "Proiect 3", description: "Dashboard React" },
+ { title: "Proiect 4", description: "Aplicatie de notite" },
+ { title: "Proiect 5", description: "Joc de trivia" }
+];
+
  return (
  <div>
- <h1>Dashboard</h1>
- <Card title="Proiect 1" description="Pagina personala cu HTML si CSS" />
- <Card title="Proiect 2" description="Pagina interactiva cu JavaScript" />
- <Card title="Proiect 3" description="Dashboard cu React" />
- </div>
+      <h1>Dashboard</h1>
+
+      {projects.map(function(item, index) {
+        return (
+          <Card key={index} title={item.title} description={item.description} />
+        );
+      })}
+
+    </div>
  );
 }
 export default App;
